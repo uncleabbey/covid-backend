b14cb1ecc51c40f20b533982dd46d1cab42d944a
Kayode Gabriel Abiodun <kayodegabriela@gmail.com>
2020-04-11 13:46:25 +0100

    node app deplo to heroku

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Server port
-var HTTP_PORT = 8000;
+var HTTP_PORT = process.env.PORT || 8000;
 // Start server
 app.listen(HTTP_PORT, () => {
   console.log('Server running on port %PORT%'.replace('%PORT%', HTTP_PORT));
